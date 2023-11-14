fec4d713b2df638f2f99dce6a6221f27a672a351
rezo <rezothebest2023@gmail.com>
2023-11-15 01:59:12 +0300

    first init

---
+++ utils/comopnents.js
@@ -0,0 +1,5 @@
+import MobileNav from "@/components/navbar/mobile/MobileNav";
+import DesktopNav from "@/components/navbar/desktop/DesktopNav";
+import Footer from "@/components/footer/Footer";
+
+export { MobileNav, DesktopNav, Footer };
